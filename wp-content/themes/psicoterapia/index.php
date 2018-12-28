9990e286e0e93a74258188d7359dd0cdf186edd6
Bia <biia.mfs@gmail.com>
2018-12-28 17:08:21 -0200

    parte laura e bia juntas

--- wp-content/themes/psicoterapia/index.php
+++ wp-content/themes/psicoterapia/index.php
@@ -1,4 +1,40 @@
 <?php get_header(); ?>
+<section>
+  <div class="row sobre-nos">
+    <div class="container">
+      <div class="row">
+        <div class="col-xs-12 text-center botao-cabecalho">
+          <a class="vem-conhecer" href="#sobre-nos-titulo">Vem conhecer!</a>
+        </div>
+
+        <div class="col-xs-12 text-center">
+          <h3 class="titulo" id="sobre-nos-titulo">Sobre nós</h3>
+        </div>
+
+        <div class="col-md-5 col-sm-6 col-md-offset-1 col-xs-12">
+          <h4 class="como-surgiu-tit">  Como surgiu? </h4>
+
+          <span class="como-surgiu-texto text-left">
+            O projeto surgiu a partir da iniciativa de um grupo de profissionais que constataram que apesar da grande procura
+            pela psicoterapia, muitos clientes não aderiram por limitações financeiras.
+          <br>
+            Como enfrentamento dessa problemática, pretendemos estruturar uma rede de profissionais engajados na cauda da saúde
+            mental e que compreendam a realidade de nossa população.
+          </span>
+        </div>
+
+        <div class="col-md-5 col-sm-6 col-xs-11 text-left container-dados-oms">
+          <span class="texto-dados-oms">
+            Temos uma demanda social bastante urgente, conforme apontam os dados fornecidos pela OMS no Brasil.
+
+            A depressão atinge 11,5 milhões de pessoas (5.8% da população), enquanto distúrbios relacionados a ansiedade afetam mais de 18.6 milhões de Brasilleiros (9.3% da população).
+          </span>
+        </div>
+
+      </div>
+    </div>
+  </div>
+</section>
 <section id="comoInscrevo" class="azulClarinho">
 
   <center><h1> Como me inscrevo? </h1> </center>
